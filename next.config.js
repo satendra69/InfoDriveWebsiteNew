914cea3dc03c167e7d928fd87e8033835045b200
satendra69 <spprtmandeep@gmail.com>
2025-12-11 21:44:36 +0800

    updated code push back

--- next.config.js
+++ next.config.js
@@ -11,10 +11,16 @@
 const nextConfig = {
   output: "export",        // Required for DigitalOcean Static Hosting
   distDir: "out",          // DO reads from /out folder
-  trailingSlash: true,     // Prevents folder routing issues
+  trailingSlash: false,     // Prevents folder routing issues
+  // Force Next.js to generate `.html` files for each page
+
   images: {
     unoptimized: true      // Required because image optimization needs a server
   },
+  experimental: {
+    appDir: false,
+  },
+  
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
