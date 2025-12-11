b88bf782aff9b891876f0fafa315751e6803e1c0
satendra69 <spprtmandeep@gmail.com>
2025-12-11 21:07:07 +0800

    updated code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
-module.exports = {
-  // output: "export",
-  // distDir: "_static",
+// module.exports = {
+//   // output: "export",
+//   // distDir: "_static",
+//   webpack: (config) => {
+//     config.resolve.fallback = { fs: false };
+//     return config;
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",        // Required for DigitalOcean Static Hosting
+  distDir: "out",          // DO reads from /out folder
+  trailingSlash: true,     // Prevents folder routing issues
+  images: {
+    unoptimized: true      // Required because image optimization needs a server
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
   },
 };
+
+module.exports = nextConfig;
+
+
